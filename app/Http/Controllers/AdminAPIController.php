bae3cdd541f85ba2ab844d833775aef8f5c51d6a
Nguyen Cong <nguyenlongit95@gmail.com>
2021-10-16 19:44:21 +0700

    tmp update

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -332,4 +332,347 @@ class AdminAPIController extends Controller
         // Export excel file by month
         return \Maatwebsite\Excel\Facades\Excel::download(new JobsExport(), 'jobs_'.Carbon::now()->format('Y-m-d').'.xlsx');
     }
+
+    /*
+     * Quan tri chung cho website
+     * Chia cac vung widgets tuong tu cho website
+     * widget sẽ được hiểu là các thành phần phụ của website
+     * */
+    public function DashBoard() 
+    {
+        return view('admin.index');
+    }
+
+    /**
+     * Logout function
+     */
+    public function logout(Request $request) 
+    {
+        Auth::logout();
+        return redirect('/admin/login');
+    }
+
+    /**
+     * Controller function list all Customer
+     */
+    public function listCustomers(Request $request) 
+    {
+        $param = $request->all();
+        $customers = $this->userRepository->listCustomers($param);
+        return view('admin.customers.index', compact('customers'));
+    }
+
+    /**
+     * Controller function list all Customer
+     */
+    public function searchCustomers(Request $request) 
+    {
+        $param = $request->all();
+        if (isset($param['create']) && $param['create'] != null) {
+            if ($this->addCustomers($request)) {
+                return redirect('/admin/customers')->with('thong_bao', 'User account creation successful');
+            }
+            return redirect('/admin/customers')->with('thong_bao', 'User account creation failed, check the system again');            
+        }
+        if (isset($param['search']) && $param['search'] != null) {
+            $customers = $this->userRepository->listCustomers($param);
+            return view('admin.customers.index', compact('customers'));
+        }
+    }
+
+    /**
+     * Controller function add new customer
+     */
+    public function addCustomers(Request $request)
+    {
+        Validation::validationUsers($request);
+        // Initialize user data
+        $param = $request->all();
+        // Init data user
+        $param['email_verified_at'] = Carbon::now();
+        $param['password'] = Hash::make($request->password);
+        $param['verified_token'] = $param['password'];
+        $param['status'] = config('const.users.status.active');
+        $param['total_file'] = 0;
+        $param['base_path'] = config('const.base_path');
+        $param['role'] = config('const.user');
+        $param['remember_token'] = Hash::make($request->password);
+        $create = $this->userRepository->create($param);
+        if ($create) {
+            // Create director in NAS storage
+            Storage::disk('ftp')->makeDirectory(config('const.base_path') . $param['name']);
+            Storage::disk('ftp')->makeDirectory(config('const.base_path') . $param['name'] . '/done');
+            return true;
+        }
+        // Error response redirect
+        return false;
+    }
+
+    /**
+     * Controller function render view edit a customer
+     */
+    public function editCustomers(Request $request, $id)
+    {
+        $customer = $this->userRepository->find($id);
+        return view('admin.customers.edit', compact('customer'));
+    }
+
+    /**
+     * Controller function update a customers
+     */
+    public function updateCustomers(Request $request, $id)
+    {
+        $customer = $this->userRepository->find($id);
+        if (empty($customer)) {
+            return redirect('/admin/customers/')->with('thong_bao', 'User account not found.');
+        }
+        Validation::validationUsers($request);
+        $param = $request->all();
+        if ($param['password'] != null) {
+            $param['password'] = Hash::make($request->password);
+        } else {
+            unset($param['password']);
+        }
+        if ($this->userRepository->update($param, $id)) {
+            return redirect('/admin/customers/edit/' . $id)->with('thong_bao', 'User account success.');
+        }
+        // Response error message
+        return redirect('/admin/customers/edit' . $id)->with('thong_bao', 'User account errors, please check again.');
+    }
+
+    /**
+     * Controller function soft delete a customer
+     */
+    public function deleteCustomers(Request $request, $id)
+    {
+        // Check customers exits
+        $customer = $this->userRepository->find($id);
+        if (empty($customer)) {
+            return redirect('/admin/customers/')->with('thong_bao', 'User account not found.');
+        }
+        // Soft delete customers
+        if ($this->userRepository->deleteCustomer($id)) {
+            return redirect('/admin/customers/')->with('thong_bao', 'User account delete success.');
+        }
+        // Response error message
+        return redirect('/admin/customers/')->with('thong_bao', 'User account delete errors, please check again.');
+    }
+
+    /**
+     * Controller function list all editors
+     */
+    public function listEditors(Request $request)
+    {
+        $param = $request->all();
+        $editors = $this->userRepository->listEditors($param);
+        return view('admin.editors.index', compact('editors'));
+    }
+
+    /**
+     * Controller function search editors
+     */
+    public function searchEditors(Request $request)
+    {
+        $param = $request->all();
+        if (isset($param['create']) && $param['create'] != null) {
+            if ($this->addEditor($request)) {
+                return redirect('/admin/editors')->with('thong_bao', 'User account creation successful');
+            }
+            return redirect('/admin/editors')->with('thong_bao', 'User account creation failed, check the system again');            
+        }
+        if (isset($param['search']) && $param['search'] != null) {
+            $editors = $this->userRepository->listEditors($param);
+            return view('admin.editors.index', compact('editors'));
+        }
+    }
+
+    /**
+     * Controller function add new customer
+     */
+    public function addEditor(Request $request)
+    {
+        Validation::validationUsers($request);
+        // Initialize user data
+        $param = $request->all();
+        // Init data user
+        $param['email_verified_at'] = Carbon::now();
+        $param['password'] = Hash::make($request->password);
+        $param['verified_token'] = $param['password'];
+        $param['status'] = config('const.users.status.active');
+        $param['total_file'] = 0;
+        $param['base_path'] = config('const.base_path');
+        $param['role'] = config('const.editor');
+        $param['remember_token'] = Hash::make($request->password);
+        $create = $this->userRepository->create($param);
+        if ($create) {
+            try {
+                // Create director in NAS storage
+                Storage::disk('ftp')->makeDirectory(config('const.base_path') . '/editors/' . $create->name . '_' . $create->id);
+                Storage::disk('ftp')->makeDirectory(config('const.base_path') . '/editors/' . $create->name . '_' . $create->id . '/done');
+                return true;
+            } catch (\Exception $e) {
+                \Log::error($e->getMessage());
+                return false;
+            }
+            return true;
+        }
+        // Error response redirect
+        return false;
+    }
+
+    /**
+     * Controller function update editors
+     */
+    public function updateEditors(Request $request, $id)
+    {
+        $editor = $this->userRepository->find($id);
+        if (empty($editor)) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Editor account not found.');
+        }
+        Validation::validationUsers($request);
+        $param = $request->all();
+        if ($param['password'] != null) {
+            $param['password'] = Hash::make($request->password);
+        } else {
+            unset($param['password']);
+        }
+        if ($this->userRepository->update($param, $id)) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Editor account success.');
+        }
+        // Response error message
+        return redirect('/admin/editors/')->with('thong_bao', 'Editor account errors, please check again.');
+    }
+
+    /**
+     * Controller function delete editor
+     */
+    public function deleteEditor(Request $request, $id)
+    {
+        // Check Editors has exits
+        $editor = $this->userRepository->find($id);
+        if (empty($editor)) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Editor account not found.');
+        }
+        // Soft delete editors
+        $param['status'] = 0;
+        if ($this->userRepository->update($param, $id)) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Editor account deleted.');
+        }
+        // Response error system
+        return redirect('/admin/editors/')->with('thong_bao', 'Editor account errors.');
+    }
+
+    /**
+     * Controller function assign jobs to editors
+     */
+    public function assignJobs(Request $request, $id)
+    {
+        // Check Editors has exits
+        $editor = $this->userRepository->find($id);
+        if (empty($editor)) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Editor account not found.');
+        }
+        // Assign jobs for editor
+        $jobs = $this->jobRepository->adminAssingJob($id);
+        if ($jobs == true) {
+            return redirect('/admin/editors/')->with('thong_bao', 'Assign jobs for editor success.');
+        }
+        // Response data errors
+        return redirect('/admin/editors/')->with('thong_bao', 'Assign jobs for editor failed system.');
+    }
+
+    /**
+     * Controller function list all QC
+     */
+    public function listQC(Request $request)
+    {
+        $param = $request->all();
+        $qcs = $this->userRepository->listQC($param);
+        return view('admin.qc.index', compact('qcs'));
+    }
+
+    /**
+     * Controller function search QC
+     */
+    public function searchQC(Request $request)
+    {
+        $param = $request->all();
+        if (isset($param['create']) && $param['create'] != null) {
+            if ($this->addQC($request)) {
+                return redirect('/admin/qc')->with('thong_bao', 'User account creation successful');
+            }
+            return redirect('/admin/qc')->with('thong_bao', 'User account creation failed, check the system again');            
+        }
+        if (isset($param['search']) && $param['search'] != null) {
+            $qcs = $this->userRepository->listQC($param);
+            return view('admin.qc.index', compact('qcs'));
+        }
+    }
+
+    /**
+     * Controller function add new customer
+     */
+    public function addQC(Request $request)
+    {
+        Validation::validationUsers($request);
+        // Initialize user data
+        $param = $request->all();
+        // Init data user
+        $param['email_verified_at'] = Carbon::now();
+        $param['password'] = Hash::make($request->password);
+        $param['verified_token'] = $param['password'];
+        $param['status'] = config('const.users.status.active');
+        $param['total_file'] = 0;
+        $param['base_path'] = config('const.base_path');
+        $param['role'] = config('const.qc');
+        $param['remember_token'] = Hash::make($request->password);
+        $create = $this->userRepository->create($param);
+        if ($create) {
+            return true;
+        }
+        // Error response redirect
+        return false;
+    }
+
+    /**
+     * Controller function edit for qc
+     */
+    public function editQC(Request $request, $id)
+    {
+        $qc = $this->userRepository->find($id);
+        if (empty($qc)) {
+            return redirect('/admin/qc/')->with('thong_bao', 'QC account not found.');
+        }
+        Validation::validationUsers($request);
+        $param = $request->all();
+        if ($param['password'] != null) {
+            $param['password'] = Hash::make($request->password);
+        } else {
+            unset($param['password']);
+        }
+        if ($this->userRepository->update($param, $id)) {
+            return redirect('/admin/qc/')->with('thong_bao', 'QC account success.');
+        }
+        // Response error message
+        return redirect('/admin/qc/')->with('thong_bao', 'QC account errors, please check again.');
+    }
+
+    /**
+     * Controller function delete QC
+     */
+    public function deleteQC(Request $request, $id)
+    {
+        // Check QC exits
+        $qc = $this->userRepository->find($id);
+        if (empty($qc)) {
+            return redirect('/admin/qc/')->with('thong_bao', 'QC account not found.');
+        }
+        // Soft delete editors
+        $param['status'] = 0;
+        if ($this->userRepository->update($param, $id)) {
+            return redirect('/admin/qc/')->with('thong_bao', 'QC account deleted.');
+        }
+        // Response error system
+        return redirect('/admin/qc/')->with('thong_bao', 'QC account errors.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,29 +27,29 @@ Route::post('/admin/login',[LoginAndRegisterController::class, 'postLogin']);
  * */
 Route::group(['prefix'=>'admin'],function() {
     // Trang DashBoard sẽ là nơi thống kê sản phẩm và các thông tin liên quan
-    Route::get('/dashboard', [\App\Http\Controllers\AdminController::class, 'dashboard']);
-    Route::get('/logout', [\App\Http\Controllers\AdminController::class, 'logout']);
+    Route::get('/dashboard', [\App\Http\Controllers\AdminAPIController::class, 'dashboard']);
+    Route::get('/logout', [\App\Http\Controllers\AdminAPIController::class, 'logout']);
 
     Route::group(['prefix' => 'customers'], function () {
-        Route::get('/', [\App\Http\Controllers\AdminController::class, 'listCustomers']);
-        Route::post('/search', [\App\Http\Controllers\AdminController::class, 'searchCustomers']);
-        Route::get('/edit/{id}', [\App\Http\Controllers\AdminController::class, 'editCustomers']);
-        Route::post('/update/{id}', [\App\Http\Controllers\AdminController::class, 'updateCustomers']);
-        Route::get('/delete/{id}', [\App\Http\Controllers\AdminController::class, 'deleteCustomers']);
+        Route::get('/', [\App\Http\Controllers\AdminAPIController::class, 'listCustomers']);
+        Route::post('/search', [\App\Http\Controllers\AdminAPIController::class, 'searchCustomers']);
+        Route::get('/edit/{id}', [\App\Http\Controllers\AdminAPIController::class, 'editCustomers']);
+        Route::post('/update/{id}', [\App\Http\Controllers\AdminAPIController::class, 'updateCustomers']);
+        Route::get('/delete/{id}', [\App\Http\Controllers\AdminAPIController::class, 'deleteCustomers']);
     });
 
     Route::group(['prefix' => 'editors'], function () {
-        Route::get('/', [\App\Http\Controllers\AdminController::class, 'listEditors']);
-        Route::post('/search', [\App\Http\Controllers\AdminController::class, 'searchEditors']);
-        Route::post('/edit/{id}', [\App\Http\Controllers\AdminController::class, 'updateEditors']);
-        Route::get('/delete/{id}', [\App\Http\Controllers\AdminController::class, 'deleteEditor']);
-        Route::get('/assign-job/{id}', [\App\Http\Controllers\AdminController::class, 'assignJobs']);
+        Route::get('/', [\App\Http\Controllers\AdminAPIController::class, 'listEditors']);
+        Route::post('/search', [\App\Http\Controllers\AdminAPIController::class, 'searchEditors']);
+        Route::post('/edit/{id}', [\App\Http\Controllers\AdminAPIController::class, 'updateEditors']);
+        Route::get('/delete/{id}', [\App\Http\Controllers\AdminAPIController::class, 'deleteEditor']);
+        Route::get('/assign-job/{id}', [\App\Http\Controllers\AdminAPIController::class, 'assignJobs']);
     });
 
     Route::group(['prefix' => 'qc'], function () {
-        Route::get('/', [\App\Http\Controllers\AdminController::class, 'listQC']);
-        Route::post('/search', [\App\Http\Controllers\AdminController::class, 'searchQC']);
-        Route::post('/edit/{id}', [\App\Http\Controllers\AdminController::class, 'editQC']);
-        Route::get('/delete/{id}', [\App\Http\Controllers\AdminController::class, 'deleteQC']);
+        Route::get('/', [\App\Http\Controllers\AdminAPIController::class, 'listQC']);
+        Route::post('/search', [\App\Http\Controllers\AdminAPIController::class, 'searchQC']);
+        Route::post('/edit/{id}', [\App\Http\Controllers\AdminAPIController::class, 'editQC']);
+        Route::get('/delete/{id}', [\App\Http\Controllers\AdminAPIController::class, 'deleteQC']);
     });
 });
