c1c64278c020b751d73162a9d42f3481cf24329b
longnct7090 <longnct7090@cowell.com.vn>
2021-12-06 11:41:13 +0700

    tmp update icon sidabar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -25,22 +25,22 @@
             <!-- Widgets dùng để quản lý các thành phần của website như header, sidebar footer -->
             <li>
                 <a href="{{ url('/admin/customers/') }}">
-                    <i class="fa fa-circle-o"></i> <span>Customers</span>
+                    <i class="fa fa-user"></i> <span>Customers</span>
                 </a>
             </li>
             <li>
                 <a href="{{ url('/admin/editors/') }}">
-                    <i class="fa fa-circle-o"></i> <span>Editors</span>
+                    <i class="fa fa-pencil"></i> <span>Editors</span>
                 </a>
             </li>
             <li>
                 <a href="{{ url('/admin/qc/') }}">
-                    <i class="fa fa-circle-o"></i> <span>QC</span>
+                    <i class="fa fa-user-circle"></i> <span>QC</span>
                 </a>
             </li>
             <li>
                 <a href="{{ url('/admin/jobs/') }}">
-                    <i class="fa fa-circle-o"></i> <span>List jobs</span>
+                    <i class="fa fa-tag"></i> <span>List jobs</span>
                 </a>
             </li>
             <li><a target="_blank" href="https://docs.google.com/document/d/15npdEv7eAbYIBWHIBrbizTHAxFuaYMnbM3m7tLBMFGE/edit?fbclid=IwAR2Vck1YYqo6ByDxpSOfCCHg0-A9kqAarme5Aeta711VtnCxmgF7bs_7q9w"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
