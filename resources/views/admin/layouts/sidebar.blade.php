f082fc83eed144a6d2ec2f56cfb384de3adf31ec
longnct7090 <longnct7090@cowell.com.vn>
2021-12-06 11:38:38 +0700

    tmp update

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -11,21 +11,15 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-       
+
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview menu-open">
+            <li>
                 <a href="{{ url('/admin/dashboard') }}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                    <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
                 </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href="{{ url('/admin/dashboard') }}"><i class="fa fa-circle-o"></i> Dashboard </a></li>
-                </ul>
             </li>
 
             <!-- Widgets dùng để quản lý các thành phần của website như header, sidebar footer -->
