c60947f91cd836137f5da665ce0d5fde7b067b03
longnct7090 <longnct7090@cowell.com.vn>
2021-12-23 14:04:51 +0700

    update session sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -72,5 +72,20 @@
     <!-- /.sidebar -->
 </aside>
 @else
-    {{ \Illuminate\Support\Facades\Auth::logout() }}
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p>Session is over</p>
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
+            </div>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
 @endif
