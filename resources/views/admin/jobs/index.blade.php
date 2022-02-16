735ad6d1d991754dcb201342370fd3caa38dfe09
longnct7090 <longnct7090@cowell.com.vn>
2022-02-16 17:21:02 +0700

    tmp update style table list jobs

--- resources/views/admin/jobs/index.blade.php
+++ resources/views/admin/jobs/index.blade.php
@@ -101,26 +101,49 @@
                             <tr>
                                 <td>{{ $job->id }}</td>
                                 <td class="text-center">{{ $carbon->format('d-m-Y') }}</td>
-                                <td class="hover-nas" onclick="showModalSelectEditor( {{ $job->id }} )">{{ $job->nas_dir }}</td>
+                                <td class="hover-nas font-weight-bold-list-job" onclick="showModalSelectEditor( {{ $job->id }} )">{{ $job->nas_dir }}</td>
                                 <td>{{ $job->user_name }}</td>
                                 <td>{{ $job->type_txt }}</td>
-                                <td>{{ $job->editor_name }}</td>
-                                <td class="text-center ">
-                                    <!-- // 0: reject, 1 chưa assign, 2 đã assign, 3 confirm, 4 done -->
-                                    @if($job->status == 0)
-                                        <span class="text-danger">
-                                    @elseif($job->status == 1)
-                                        <span>
-                                    @elseif($job->status == 2)
-                                        <span class="text-green">
-                                    @elseif($job->status == 3)
-                                        <span class="text-warning">
-                                    @else
-                                        <span class="text-success text-bold">
-                                    @endif
+                                <td class="font-weight-bold-list-job">{{ $job->editor_name }}</td>
+                                <!-- // 0: reject, 1 chưa assign, 2 đã assign, 3 confirm, 4 done -->
+                                @if($job->status == 0)
+                                    <td class="text-center font-weight-bold bg-danger">
+                                        <span class="color-white">{{ $job->status_txt }}</span>
+                                    </td>
+                                @elseif($job->status == 1)
+                                    <td class="text-center font-weight-bold">
                                         {{ $job->status_txt }}
-                                    </span>
-                                </td>
+                                    </td>
+                                @elseif($job->status == 2)
+                                    <td class="text-center font-weight-bold bg-green">
+                                        <span class="color-white">{{ $job->status_txt }}</span>
+                                    </td>
+                                @elseif($job->status == 3)
+                                    <td class="text-center font-weight-bold bg-warning">
+                                        <span class="color-white">{{ $job->status_txt }}</span>
+                                    </td>
+                                @else
+                                    <td class="text-center font-weight-bold bg-success">
+                                        <span class="color-white">{{ $job->status_txt }}</span>
+                                    </td>
+                                @endif
+
+{{--                                <td class="text-center font-weight-bold bg-danger">--}}
+{{--                                    <!-- // 0: reject, 1 chưa assign, 2 đã assign, 3 confirm, 4 done -->--}}
+{{--                                    @if($job->status == 0)--}}
+{{--                                        <span class="text-danger">--}}
+{{--                                    @elseif($job->status == 1)--}}
+{{--                                        <span>--}}
+{{--                                    @elseif($job->status == 2)--}}
+{{--                                        <span class="text-green">--}}
+{{--                                    @elseif($job->status == 3)--}}
+{{--                                        <span class="text-warning">--}}
+{{--                                    @else--}}
+{{--                                        <span class="text-success text-bold">--}}
+{{--                                    @endif--}}
+{{--                                        {{ $job->status_txt }}--}}
+{{--                                    </span>--}}
+{{--                                </td>--}}
                                 <td>{{ $job->note }}</td>
                                 <td>{{ $job->customer_note }}</td>
                                 <td class="text-center">
@@ -180,6 +203,24 @@
         }
         .bg-table-list-job {
             background: #00000015;
+        }
+        .bg-green {
+            background: #00a65a;
+        }
+        .bg-success {
+            background: #3c763d;
+        }
+        .bg-danger {
+            background: #a94442;
+        }
+        .bg-warning {
+            background: #f39c12eb;
+        }
+        .font-weight-bold-list-job {
+            font-weight: bold !important;
+        }
+        .color-white {
+            color: white !important;
         }
     </style>
 @endsection
